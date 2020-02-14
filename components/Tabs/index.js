2f43e124f4355ec53132e7411bbe46c25de4b7fa
MrR3set <bairon.pazrobles@gmail.com>
2020-02-14 08:44:01 -0800

    tabs and header done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const topics = document.getElementsByClassName("topics")[0];
+
+//loop through all the topics and add each topic
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(response => {
+    response.data.topics.forEach(topic => {
+        topics.append(createTab(topic));
+    })
+}).catch(err => { console.log(`${err}`) });
+
+function createTab(topic) {
+    const tab = document.createElement("div"); //declaration
+    tab.classList.add("tab"); //Class assig
+    tab.textContent = topic;
+
+    return tab;
+}
