7c9fbc492826d31c9f014e69cb5afc2dba302758
MrR3set <bairon.pazrobles@gmail.com>
2020-02-14 09:54:09 -0800

    mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+const cardsContainer = document.getElementsByClassName("cards-container")[0];
+
+let dataArray = [];
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(response => {
+    Object.values(response.data.articles).forEach(topic => {
+        topic.forEach(article => {
+            cardsContainer.append(createArticle(article))
+        })
+    })
+})
+
+
+function createArticle(topic) {
+    //declaration
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgcontainer = document.createElement("div");
+    const img = document.createElement("img");
+    const authorName = document.createElement("span");
+    //class assignation
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgcontainer.classList.add("img-container");
+    //appending
+    imgcontainer.append(img);
+    author.append(imgcontainer, authorName);
+    card.append(headline, author);
+    //data assignation
+    headline.textContent = topic.headline;
+    img.src = topic.authorPhoto;
+    authorName.textContent = topic.authorName;
+
+    return card;
+}
